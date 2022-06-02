8788dd995a60e46998741c2710f47627b07d838a
James Douglass <jdouglass@stanford.edu>
2022-06-02 16:54:46 -0700

    Specifying bash time program and adding executable bit.

--- backend-experiments/07-read-block-and-build-vrt/doit.sh
+++ backend-experiments/07-read-block-and-build-vrt/doit.sh
@@ -2,5 +2,5 @@
 
 for i in {1..10}
 do
-    time gdalbuildvrt -overwrite nlud.vrt /vsigs/natcap-urban-online-datasets/nlud.tif
+    /usr/bin/time gdalbuildvrt -overwrite nlud.vrt /vsigs/natcap-urban-online-datasets/nlud.tif
 done
