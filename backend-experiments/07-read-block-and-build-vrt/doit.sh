0c0a9482c92becd406e063270729fd690c39b85a
James Douglass <jdouglass@stanford.edu>
2022-06-02 16:56:17 -0700

    Using portable output instead.

--- backend-experiments/07-read-block-and-build-vrt/doit.sh
+++ backend-experiments/07-read-block-and-build-vrt/doit.sh
@@ -2,5 +2,5 @@
 
 for i in {1..10}
 do
-    /usr/bin/time gdalbuildvrt -overwrite nlud.vrt /vsigs/natcap-urban-online-datasets/nlud.tif
+    /usr/bin/time --portability gdalbuildvrt -overwrite nlud.vrt /vsigs/natcap-urban-online-datasets/nlud.tif
 done
