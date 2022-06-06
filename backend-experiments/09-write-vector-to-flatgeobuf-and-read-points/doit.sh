46e911af9711239013e2869a032d59595de4c6fa
James Douglass <jdouglass@stanford.edu>
2022-06-06 13:24:29 -0700

    Adding tests for geopackage for another point of reference.

--- backend-experiments/09-write-vector-to-flatgeobuf-and-read-points/doit.sh
+++ backend-experiments/09-write-vector-to-flatgeobuf-and-read-points/doit.sh
@@ -32,6 +32,28 @@ do
     /usr/bin/time --portability  python search-by-bounding-box.py "$VRT"
 done
 
+ogr2ogr -f GPKG -progress random_global_points_WSI.gpkg random_global_points_WSI.fgb
+gsutil cp ./*.gpkg "gs://$BUCKET"
 
+# Time local accesses for reference.
+echo "\nTiming local file accesses (base case)"
+for filename in ls *.gpkg
+do
+    /usr/bin/time --portability  python search-by-bounding-box.py "$filename"
+done
 
+# time remote access
+echo "\nTiming access over the network"
+for filename in ls *.gpkg
+do
+    /usr/bin/time --portability  python search-by-bounding-box.py "/vsigs/$BUCKET/$filename"
+done
 
+# Try out a VRT
+echo "\nVRT access via VSIGS"
+for filename in ls *.gpkg
+do
+    VRT="$filename.vt"
+    gdalbuildvrt "$VRT" "/vsigs/$BUCKET/$filename"
+    /usr/bin/time --portability  python search-by-bounding-box.py "$VRT"
+done
