b110913b8b220b029aa348cfdcdca7908d223447
davemfish <davemfish@gmail.com>
2022-06-15 16:12:04 -0400

    removed unused prop. #3.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,6 @@ export default function App() {
           setParcel={setParcel}
         />
         <EditMenu
-          open={true}
           parcel={parcel}
           refreshSavedScenarios={refreshSavedScenarios}
           savedScenarios={savedScenarios}
