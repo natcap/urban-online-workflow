2c34c3111dee3418217c2311edb84ef120c35148
James Douglass <jdouglass@stanford.edu>
2022-06-16 14:34:25 -0500

    Adding a script to build COG of Chris's NLCD.

---
+++ backend-experiments/10-convert-nlcd-to-epsg3857/doit.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+set -ex
+
+NLCD=NLCD_2016_Land_Cover_L48_20190424.img
+
+# GDAL needs these files to be available at specific filenames
+# cp -n returns exit code of 1 when file exists.
+cp -n NLCD_2016_Land_Cover_L48_20190424-001.ige NLCD_2016_Land_Cover_L48_20190424.ige || true
+cp -n NLCD_2016_Land_Cover_L48_20190424-002.rde NLCD_2016_Land_Cover_L48_20190424.rde || true
+WARPED_NLCD=NLCD_2016_epsg3857.tif.tmp
+FINAL_NLCD=NLCD_2016_epsg3857.tif
+gdalwarp \
+    -t_srs "EPSG:3857" \
+    -r near \
+    -multi \
+    -overwrite \
+    -of "GTiff" \
+    -co "BIGTIFF=YES" \
+    -co "COMPRESS=DEFLATE" \
+    -co "TILED=YES" \
+    -co "NUM_THREADS=ALL_CPUS" \
+    "$NLCD" "$WARPED_NLCD"
+
+gdaladdo "$WARPED_NLCD"
+
+gdal_translate \
+    -of "COG" \
+    -co "BIGTIFF=YES" \
+    -co "COMPRESS=DEFLATE" \
+    -co "LEVEL=9" \
+    -co "COPY_SRC_OVERVIEWS=YES" \
+    -co "NUM_THREADS=ALL_CPUS" \
+    -co "ADD_ALPHA=YES" \
+    "$WARPED_NLCD" "$FINAL_NLCD"
+
+rm "$WARPED_NLCD"
