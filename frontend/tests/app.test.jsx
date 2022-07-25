13a76937573962b33989fbb4e7100f3bb215500c
davemfish <davemfish@gmail.com>
2022-07-25 16:32:54 -0400

    remove some unused imports. #8

--- frontend/tests/app.test.jsx
+++ frontend/tests/app.test.jsx
@@ -1,8 +1,7 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import React from 'react';
 import {
   render,
-  screen,
 } from '@testing-library/react';
 
 import App from '../src/App';
