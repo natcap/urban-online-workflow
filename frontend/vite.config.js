338732ccf3465e2b549237b1836e0dc64db39fd6
James Douglass <jdouglass@stanford.edu>
2022-09-23 12:12:12 -0700

    Removing the public dir from vite - nginx is serving these files now.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,18 +6,13 @@ import react from '@vitejs/plugin-react';
 // is running in docker.
 let envDir = '.';
 
-// We need public files to be written to a known place.
-// Within docker, this is /opt/appdata.  Locally, it's in ../appdata.
-let appdataDir = '../appdata/';
 if (process.env.MODE === 'docker') {
   envDir = '/run/secrets/';
-  appdataDir = '/opt/appdata/';
 }
 
 const CONFIG = {
   plugins: [react()],
   envDir: envDir,
-  publicDir: appdataDir,
   test: {
     globals: true,
     environment: 'happy-dom',
