6be1c122b24403dddd4b73a920853b2de7706dc8
davemfish <davemfish@gmail.com>
2023-04-19 16:36:59 -0400

    track invest model progress on frontend

--- frontend/src/edit/investRunner.jsx
+++ frontend/src/edit/investRunner.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import {
   Button,
+  ProgressBar,
 } from '@blueprintjs/core';
 
 import useInterval from '../hooks/useInterval';
@@ -16,6 +17,9 @@ export default function InvestRunner(props) {
     refreshScenarios,
   } = props;
   const [jobIDs, setJobIDs] = useState([]);
+  const [progress, setProgress] = useState(0);
+  const [nJobs, setNJobs] = useState(null);
+  const [progressState, setProgressState] = useState('success');
 
   useInterval(async () => {
     const statuses = await Promise.all(jobIDs.map((id) => getJobStatus(id)));
@@ -23,25 +27,44 @@ export default function InvestRunner(props) {
     statuses.forEach((status, idx) => {
       if (['success', 'failed'].includes(status)) {
         pendingJobs.splice(idx, 1);
+        setProgress((nJobs - pendingJobs.length) / nJobs);
+        if (status === 'failed') { setProgressState('warning'); }
       }
     });
     setJobIDs(pendingJobs);
     if (!pendingJobs.length) {
-      refreshScenarios();
+      refreshScenarios(); // assuming results are in scenario db
     }
   }, (jobIDs.length) ? 2000 : null);
 
   const handleClick = async () => {
-    const jobs = await Promise.all(scenarios.map((scenario) => runInvest(scenario.scenario_id)));
-    const jids = jobs.map((j) => Object.values(j));
-    setJobIDs(jids.flat());
+    setProgress(0);
+    const jobs = await Promise.all(
+      scenarios.map((scenario) => runInvest(scenario.scenario_id))
+    );
+    const jids = jobs.map((j) => Object.values(j)).flat();
+    setJobIDs(jids);
+    setNJobs(jids.length);
   };
 
   return (
-    <Button
-      onClick={handleClick}
-    >
-      Run InVEST Models
-    </Button>
+    <div id="invest-runner">
+      <Button
+        onClick={handleClick}
+      >
+        Run InVEST Models
+      </Button>
+      {
+        (jobIDs.length || progress === 1)
+          ? (
+            <ProgressBar
+              value={progress}
+              intent={progressState}
+              animate={false}
+            />
+          )
+          : <div />
+      }
+    </div>
   );
 }
