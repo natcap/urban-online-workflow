bf3a47568c83cf8b305df0c17ef0e1ac859df149
davemfish <davemfish@gmail.com>
2024-03-18 14:01:47 -0400

    add a units selector for results metrics. #93

--- frontend/src/edit/results.jsx
+++ frontend/src/edit/results.jsx
@@ -16,22 +16,32 @@ import {
 const METRICS = {
   avg_tmp_v: {
     label: 'change in temperature',
-    units: `${'\u00b0'}F`,
+    units: {
+      [`${'\u00b0'}F`]: (x) => x,
+    },
     precision: 2,
   },
   cdd_cost: {
     label: 'change in cooling cost',
-    units: 'USD',
+    units: {
+      USD: (x) => x,
+    },
     precision: 0,
   },
   tot_c_cur: {
     label: 'change in carbon stored',
-    units: 'metric tons',
+    units: {
+      'metric tons': (x) => x,
+    },
     precision: 0,
   },
   nature_supply_percapita: {
-    label: 'change in supply of nature',
-    units: 'square meters per person',
+    label: 'change in supply of nature per person',
+    units: {
+      'square meters': (x) => x,
+      'square feet': (x) => x * 10.764,
+      'acres': (x) => x / 4016.856,
+    },
     precision: 1,
   },
 };
@@ -75,15 +85,15 @@ function ResultsDescription(props) {
     scenarioName,
   } = props;
 
-  const temperature = deltaTable[scenarioName]['avg_tmp_v'].value;
-  const coolingCost = deltaTable[scenarioName]['cdd_cost'].value;
+  const temperature = deltaTable[scenarioName]['avg_tmp_v'];
+  const coolingCost = deltaTable[scenarioName]['cdd_cost'];
   const tempDirection = (temperature >= 0) ? 'increase' : 'decrease';
 
-  const carbon = deltaTable[scenarioName]['tot_c_cur'].value;
+  const carbon = deltaTable[scenarioName]['tot_c_cur'];
   const carbonDirection = (carbon >= 0) ? 'increase' : 'decrease';
 
   const natureSupplyBase = results['baseline']['nature_supply_percapita'];
-  const natureSupplyDelta = deltaTable[scenarioName]['nature_supply_percapita'].value;
+  const natureSupplyDelta = deltaTable[scenarioName]['nature_supply_percapita'];
   const natureSupplyPercentChange = (natureSupplyDelta / natureSupplyBase) * 100;
   const natureDirection = (natureSupplyDelta >= 0) ? 'increase' : 'decrease';
   const natureBalance = results['baseline']['ntr_bal_avg'];
@@ -136,13 +146,84 @@ function ResultsDescription(props) {
   );
 }
 
+function ResultsTable(props) {
+  const {
+    deltaTable,
+    scenarioNames,
+  } = props;
+
+  const [metricLookup, setMetricLookup] = useState(null);
+
+  useEffect(() => {
+    const data = {};
+    Object.keys(METRICS).forEach((metric) => {
+      data[metric] = {
+        unit: Object.keys(METRICS[metric].units)[0],
+        func: Object.values(METRICS[metric].units)[0],
+      };
+    });
+    setMetricLookup(data);
+  }, []);
+
+  const setUnit = (unit, metric) => {
+    const lookup = { ...metricLookup };
+    lookup[metric].unit = unit;
+    lookup[metric].func = METRICS[metric].units[unit];
+    setMetricLookup(lookup);
+  };
+
+  if (metricLookup) {
+    return (
+      <HTMLTable>
+        <tbody>
+          <tr key="header">
+            <th key="blank" />
+            {Object.entries(METRICS).map(([metric, obj]) => (
+              <th key={obj.label}>
+                {obj.label}
+                <br />
+                {(Object.keys(obj.units).length > 1)
+                  ? (
+                    <HTMLSelect
+                      value={metricLookup[metric].unit}
+                      onChange={(e) => setUnit(e.target.value, metric)}
+                    >
+                      {Object.keys(obj.units).map(
+                        (unit) => <option key={unit} value={unit}>{unit}</option>
+                      )}
+                    </HTMLSelect>
+                  )
+                  : <span className="units">({Object.keys(obj.units)[0]})</span>}
+              </th>
+            ))}
+          </tr>
+          {scenarioNames.map((name) => (
+            <tr key={name}>
+              <th>{name}</th>
+              {Object.keys(metricLookup).map((metric) => {
+                const value = metricLookup[metric]['func'](
+                  deltaTable[name][metric],
+                ).toFixed(METRICS[metric].precision);
+                return (
+                  <td className="table-value" key={metric}>
+                    {(value > 0) ? `+${value}` : value}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </HTMLTable>
+    );
+  }
+}
+
 export default function Results(props) {
   const {
     results,
     scenarioDescriptions,
     setSelectedScenario,
   } = props;
-  console.log(results)
 
   const [scenarioName, setScenarioName] = useState(null);
   const [deltaTable, setDeltaTable] = useState(null);
@@ -158,13 +239,8 @@ export default function Results(props) {
     const names = Object.keys(results).filter((key) => key !== 'baseline');
     names.forEach((name) => {
       data[name] = {};
-      Object.entries(METRICS).forEach(([metric, obj]) => {
-        data[name][metric] = {
-          value: results[name][metric] - results['baseline'][metric],
-          units: obj.units,
-          precision: obj.precision,
-          label: obj.label,
-        };
+      Object.keys(METRICS).forEach((metric) => {
+        data[name][metric] = results[name][metric] - results['baseline'][metric];
       });
     });
     setDeltaTable(data);
@@ -215,27 +291,11 @@ export default function Results(props) {
               <p>
                 Each scenario's impact relative to baseline conditions:
               </p>
-              <HTMLTable id="results-matrix">
-                <tbody>
-                  <tr key="header">
-                    <th key="blank" />
-                    {Object.values(METRICS).map((obj) => (
-                      <th key={obj.label}>{obj.label}</th>
-                    ))}
-                  </tr>
-                  {scenarioNames.map((name) => (
-                    <tr key={name}>
-                      <th>{name}</th>
-                      {Object.values(deltaTable[name]).map((obj) => (
-                        <td className="table-value" key={obj.units}>
-                          {(obj.value > 0) ? `+${obj.value.toFixed(obj.precision)}` : obj.value.toFixed(obj.precision)}
-                          <span className="units">{obj.units}</span>
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </HTMLTable>
+              <ResultsTable
+                id="results-matrix"
+                deltaTable={deltaTable}
+                scenarioNames={scenarioNames}
+              />
             </div>
           ) : <div />
       }
