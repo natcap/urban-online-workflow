0a203e2c849d01b7e294aa0f12322f25a3254f24
davemfish <davemfish@gmail.com>
2024-09-06 12:25:31 -0400

    some cleanup in stylesheet. #113

--- frontend/src/edit/scenarioBuilder.jsx
+++ frontend/src/edit/scenarioBuilder.jsx
@@ -5,6 +5,7 @@ import {
   InputGroup,
   Radio,
   RadioGroup,
+  Section,
   Spinner
 } from '@blueprintjs/core';
 
@@ -41,7 +42,6 @@ export default function ScenarioBuilder(props) {
   useInterval(async () => {
     // There are sometimes two jobs submitted concurrently.
     // They are in a priority queue, so for now monitor the lower priority one.
-    console.log('checking status for job', jobID);
     const status = await getJobStatus(jobID);
     if (status === 'success') {
       refreshScenarios();
@@ -83,7 +83,7 @@ export default function ScenarioBuilder(props) {
         Create a scenario:
       </label>
       <p><em>choose new landuse and landcover for the study area</em></p>
-      <RadioGroup
+      {/*<RadioGroup
         className="conversion-radio"
         inline
         onChange={(event) => setConversionOption(event.target.value)}
@@ -91,7 +91,7 @@ export default function ScenarioBuilder(props) {
       >
         <Radio key="fill" value="fill" label="fill" />
         <Radio key="wallpaper" value="wallpaper" label="wallpaper" disabled/>
-      </RadioGroup>
+      </RadioGroup>*/}
       <div>
         {
           (conversionOption === 'fill')
@@ -113,9 +113,6 @@ export default function ScenarioBuilder(props) {
         }
       </div>
       <div id="scenario-input-label">
-        {/*TODO: This subscript is a note to self, related to
-        https://github.com/natcap/urban-online-workflow/issues/124*/}
-        <sub>{singleLULC}</sub>
         {
           (jobID)
             ? <Spinner size="20" />
